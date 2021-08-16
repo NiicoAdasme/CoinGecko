22a162594a6ba071db5efb02f4f0412355d2bee8
NiicoAdasme <nicolas.wladimir@gmail.com>
2021-08-16 19:45:04 -0400

    deploy

--- src/components/CoinRow.jsx
+++ src/components/CoinRow.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 
-export const CoinRow = () => {
+export const CoinRow = ({ coin, index }) => {
+
     return (
-        <div>
-            
-        </div>
+        <tr>
+            <td>{index + 1}</td>
+            <td>
+                <img src={coin.image} alt={coin.name} style={{ width: '5%' }} className='img-fluid me-4' />
+                <span>{coin.name}</span>
+                <span className='text-muted ms-3 text-uppercase'>{coin.symbol}</span>
+            </td>
+            <td>{coin.current_price}</td>
+            <td className={coin.price_change_percentage_24h > 0 ? 'text-success' : 'text-danger'}>
+                {coin.price_change_percentage_24h}
+            </td>
+            <td>{coin.total_volume}</td>
+        </tr>
     )
 }
--- src/components/TableCoins.jsx
+++ src/components/TableCoins.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { CoinRow } from './CoinRow';
 
 export const TableCoins = ({ coins, search }) => {
 
@@ -22,19 +23,7 @@ export const TableCoins = ({ coins, search }) => {
             <tbody>
                 {
                     filteredCoins.map((coin, index) => (
-                        <tr key={index}>
-                            <td>{index+1}</td>
-                            <td>
-                                <img src={coin.image} alt={coin.name} style={{width:'5%'}} className='img-fluid me-4'/>
-                                <span>{coin.name}</span>
-                                <span className='text-muted ms-3 text-uppercase'>{coin.symbol}</span>
-                            </td>
-                            <td>{coin.current_price}</td>
-                            <td className={coin.price_change_percentage_24h > 0 ? 'text-success' : 'text-danger'}>
-                                {coin.price_change_percentage_24h}
-                            </td>
-                            <td>{coin.total_volume}</td>
-                        </tr>
+                        <CoinRow coin={coin} index={index} key={index} />
                     ))
                 }
             </tbody>
